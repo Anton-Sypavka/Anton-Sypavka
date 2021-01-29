c6cde21bf664f5282add957c6d7d7adcca99b614
Anton Sypavka <anton.sypavka@gmail.com>
2021-01-29 17:18:54 +0200

    Stopwatch: done

---
+++ Stopwatch/src/components/Stopwatch/index.js
@@ -0,0 +1,59 @@
+import React, {useState, useEffect} from 'react';
+
+import {interval, Subject, from, Observable,} from "rxjs";
+import { takeUntil, debounce, create, share} from "rxjs/operators";
+
+import './styles.scss';
+
+const Stopwatch = () => {
+    const [sec, setSec] = useState(0);
+    const [status, setStatus] = useState(false);
+
+    useEffect( () => {
+        const unsubscribe$ = new Subject();
+        interval(1000)
+            .pipe(takeUntil(unsubscribe$))
+            .subscribe( () => {
+                if (status) {
+                    setSec( value => value + 1000)
+                }
+            })
+        return () => {
+            unsubscribe$.next();
+            unsubscribe$.complete();
+        }
+    }, [status]);
+
+    const start = () => {
+        setStatus(!status)
+    }
+
+    const stop = () => {
+        setStatus(!status)
+        setSec(0)
+    }
+
+    const reset = () => {
+        setSec(0)
+    }
+
+    const wait = () => {
+        setStatus(!status)
+    }
+
+    return (
+        <div className='container'>
+            <span> {new Date(sec).toISOString().slice(11, 19)}</span>
+            <div className='buttons-container'>
+                {status
+                    ? <button onClick={stop}>Stop</button>
+                    : <button onClick={start}>Start</button>
+                }
+                <button onClick={reset}>Reset</button>
+                <button onClick={wait}>Wait</button>
+            </div>
+        </div>
+    )
+}
+
+export default Stopwatch;
